1aefd2edcd2d9e4e40d9324964b57c3df06bbfd4
teyper <teyjhanabartonneal@spelman.edu>
2024-12-10 11:57:52 -0500

    redo

--- Assets/Assets/ASSEST LINKS.cs
+++ Assets/Assets/ASSEST LINKS.cs
@@ -13,9 +13,11 @@ public class ASSESTLINKS : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Player link:
+        //Player link:https://drive.google.com/drive/folders/1UeDifaquHst7ZBlGwGv_-zT4VyzdkA8N
         //bullet link: https://www.pixilart.com/draw/lazer-bullet-10ba0a0082df430# 
-        //music link: 
+        //music link:https://www.youtube.com/watch?v=bBsKplb2E6Q 
+        //laser sound: https://freesound.org/people/Smoker858/sounds/508854/
+        //BG image: https://www.deviantart.com/bulwarken/art/Pixel-Art-Grass-692409685
 
     }
 }
--- Assets/SOLDIER/SoldierScript.cs
+++ Assets/SOLDIER/SoldierScript.cs
@@ -6,75 +6,122 @@ public class SoldierScript : MonoBehaviour
 {
     [SerializeField] float Speed = 2f;
     [SerializeField] float Ymin = -4f; // lower boundary 
-    [SerializeField] float Ymax = 4f; // upper boundary
+    [SerializeField] float Ymax = 4f; //upper boundary
     [SerializeField] float Xmin = -6.5f;
     [SerializeField] float Xmax = 6.5f;
-    [SerializeField] GameObject laserPrefab; // bullet
-    // Create animator reference 
+    [SerializeField] GameObject laserPrefab; //bullet
+    //create ani referenece 
     Animator animator;
     SpriteRenderer spriteRenderer;
 
-    // Laser direction
+    //rotation of bullet
+    //bool laserRotation; 
+
+    //laser direction
     private Vector3 laserDirection = Vector3.down;
+    // Start is called before the first frame update
 
     void Start()
     {
         animator = GetComponent<Animator>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer = GetComponent < SpriteRenderer>();
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-
-        // Horizontal movement and flipping logic
-        if (x > 0) // Moving right
+        //walk up
+        if(Input.GetKeyDown(KeyCode.UpArrow))
         {
-            animator.SetBool("WalkRight", true);
-            spriteRenderer.flipX = false; // Don't flip when moving right
-            laserDirection = Vector3.right;
-        }
-        else if (x < 0) // Moving left
-        {
-            animator.SetBool("WalkRight", true);
-            spriteRenderer.flipX = true; // Flip when moving left
-            laserDirection = Vector3.left;
-        }
-        else
-        {
-            animator.SetBool("WalkRight", false); // Stop horizontal movement animation
-        }
-
-        // Vertical movement logic
-        if (y > 0) // Moving up
-        {
+            //runs when up arrow is presssed
             animator.SetBool("WalkUp", true);
             laserDirection = Vector3.up;
         }
-        else if (y < 0) // Moving down
+        if(Input.GetKeyUp(KeyCode.UpArrow))
         {
+            //runs when up arrow is released
+            animator.SetBool("WalkUp", false);
+        }
+
+        //walk down
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            //runs when up arrow is presssed
             animator.SetBool("WalkDown", true);
             laserDirection = Vector3.down;
         }
-        else
+        if (Input.GetKeyUp(KeyCode.DownArrow))
         {
-            animator.SetBool("WalkUp", false); // Stop vertical movement animation
-            animator.SetBool("WalkDown", false); // Stop vertical movement animation
+            //runs when up arrow is released
+            animator.SetBool("WalkDown", false);
+        }
+        //walk right 
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            //runs when up arrow is presssed
+            animator.SetBool("WalkRight", true);
+            laserDirection = Vector3.right;
+            spriteRenderer.flipX = false;
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            //runs when up arrow is released
+            animator.SetBool("WalkRight", false);
         }
 
-        // Move the player
-        transform.Translate(x * Speed * Time.deltaTime, y * Speed * Time.deltaTime, 0f);
+        //walk left 
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            //runs when up arrow is presssed
+            animator.SetBool("WalkRight", true);
+            spriteRenderer.flipX = true;
+            laserDirection = Vector3.left;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            //runs when up arrow is released
+            animator.SetBool("WalkRight", false);
+            spriteRenderer.flipX = true;
+        }
+        
+ 
+       
+        //transforming the little guy
 
-        // Boundary enforcement
-        if (transform.position.y > Ymax) transform.position = new Vector3(transform.position.x, Ymax, transform.position.z);
-        if (transform.position.y < Ymin) transform.position = new Vector3(transform.position.x, Ymin, transform.position.z);
-        if (transform.position.x > Xmax) transform.position = new Vector3(Xmax, transform.position.y, transform.position.z);
-        if (transform.position.x < Xmin) transform.position = new Vector3(Xmin, transform.position.y, transform.position.z);
-    
+        float y;
+        float x;
 
 
-if (Input.GetKeyDown(KeyCode.Space))  // instatiate laser
+        y = Input.GetAxis("Vertical");
+        x = Input.GetAxis("Horizontal");
+
+
+        transform.Translate(0f, y * Speed * Time.deltaTime, 0f);
+        transform.Translate(x * Speed * Time.deltaTime, 0f, 0f);
+
+        // boundary above Ymax
+        if (transform.position.y > Ymax)
+        {
+            transform.position = new Vector3(transform.position.x, Ymax, transform.position.z);
+        }
+        //boundary below Ymin
+        if (transform.position.y < Ymin)
+        {
+            transform.position = new Vector3(transform.position.x, Ymin, transform.position.z);
+        }
+        // boundary outside Xmax
+        if (transform.position.x > Xmax)
+        {
+            transform.position = new Vector3(Xmax, transform.position.y, transform.position.z);
+        }
+        //boundary outside Xmin
+        if (transform.position.x < Xmin)
+        {
+            transform.position = new Vector3(Xmin, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))  // instatiate laser
         {
             GameObject obj;
             obj = Instantiate(laserPrefab);
